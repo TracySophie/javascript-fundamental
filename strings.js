3e7227492ca83765f7949fd7fad52d1958e412aa
TracySophie <tracysophie2003@gmail.com>
2022-10-19 09:25:45 +0300

    second commit

---
+++ strings.js
@@ -0,0 +1,36 @@
+let animal = "dog";
+console.log(animal.length);
+
+//acces individual characters in a string
+ console.log(animal[3]);
+
+ let msg ="i am king";
+ console.log(msg);
+ let yellMsg = msg. toUpperCase();
+ console.log(yellMsg);
+
+ let angry ="LeAVE  Me aLoNe";
+ let lower = angry.toLowerCase();
+ console.log(lower);
+
+//  trim method
+let greetings = 'leave me alone please';
+console.log(greetings);
+ let newGreetings = greetings.trim();
+ console.log(newGreetings);
+
+ let tvShow ='catdog'
+ let firstIndex = tvShow.indexOf('cat');
+ console.log(firstIndex);
+//  slice
+let str = 'lokichoggio';
+console.log(str);
+
+let partOfStr = str.slice(2,6);
+console.log(partOfStr);
+
+// replace
+let annoyingLaugh = 'teehee so funny! teehee';
+console.log(annoyingLaugh);
+ let replacer = annoyingLaugh.replace('teehee', 'haha')
+
