e816c24a770a7ce055177b72731bb731aa4bc4ff
TracySophie <tracysophie2003@gmail.com>
2022-11-07 12:57:43 +0300

    fifth commit

---
+++ madlib.js
@@ -0,0 +1,7 @@
+function madlib(adjective, noun, verb, adverb){
+  return`The ${adjective} ${noun} ${verb} too ${adverb}`;  
+}
+ let mad= madlib('dark', 'boy','run', 'quickly');
+ console.log(mad);
+
+ 
